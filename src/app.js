430b8d0904b6cd2c45c3af6b81eadabd3cf67261
tobudim <tobudim@protonmail.com>
2020-07-30 14:25:09 +0200

    initialize router and manage 404

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.port || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  return res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.use(function (req, res) {
+  return res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
