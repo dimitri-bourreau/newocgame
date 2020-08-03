ed5bbfca7ec676b535010b2482423fd0714dba9c
tobudim <tobudim@mdp>
2020-08-03 16:36:32 +0200

    update server to get params

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const port = process.env.port || 3000;
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.get('/:width?/:length?', (req, res, next) => {
+  if (typeof req.params.width !== 'undefined' && typeof req.params.length !== 'undefined') {
+    const { width, length } = req.params;
+    const valuesPasses = checkWidthAndHeight(width, length);
+    if (!valuesPasses) return next();
+    return res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  }
   return res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
@@ -15,5 +21,13 @@ app.use(function (req, res) {
 });
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 );
+
+function checkWidthAndHeight(width, length) {
+  const allowedValues = [5, 10, 15, 20];
+  if (allowedValues.includes(width) && allowedValues.includes(length)) {
+    return true;
+  }
+  return false;
+}
