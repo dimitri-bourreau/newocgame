1c607a082cded32aa2e93dbc07e6b7e29fd9222e
tobudim <tobudim@mdp>
2020-08-12 23:27:20 +0200

    update initiateBoard so that it will also insert players's data

--- src/public/scripts/game/initiate-board.mjs
+++ src/public/scripts/game/initiate-board.mjs
@@ -1,10 +1,18 @@
 import { getBoardDimensions } from './board-game/get-board-dimensions.mjs';
 import { makeSomeSquaresBlack } from './board-game/make-some-squares-black.mjs';
 
-function initiateBoardGame() {
+function drawBoard() {
   const dimensions = getBoardDimensions();
   makeSomeSquaresBlack(dimensions);
-  return null;
+}
+
+function insertPlayers({ player1, player2 }) {
+  console.log(player1, player2);
+}
+
+function initiateBoardGame(players) {
+  drawBoard();
+  insertPlayers(players);
 }
 
 export { initiateBoardGame };
--- src/public/scripts/main.mjs
+++ src/public/scripts/main.mjs
@@ -3,7 +3,11 @@ import { initiatePlayers } from './game/initiate-players.mjs';
 import { initiateWeapons } from './game/initiate-weapons.mjs';
 import { listenToResetBtn } from './game/reset-game.mjs';
 
-listenToResetBtn('restart-game-btn');
-initiateBoardGame();
 const weapons = initiateWeapons();
 const players = initiatePlayers(weapons);
+
+// Initiate
+listenToResetBtn('restart-game-btn');
+initiateBoardGame(players);
+
+// Game
