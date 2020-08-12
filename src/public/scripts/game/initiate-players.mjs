cef074651afa6dffaea02e931f637f6d8add67a7
tobudim <tobudim@mdp>
2020-08-12 23:27:00 +0200

    fix typo

--- src/public/scripts/game/initiate-players.mjs
+++ src/public/scripts/game/initiate-players.mjs
@@ -3,7 +3,7 @@ import { Player } from '../classes/player.mjs';
 function initiatePlayers({ fists }) {
   return {
     player1: new Player({ name: 'Thomas Pesquet', picture: '/pesquet.png', weapon: fists }),
-    player1: new Player({ name: 'Jack Bauer', picture: '/bauer.png', weapon: fists }),
+    player2: new Player({ name: 'Jack Bauer', picture: '/bauer.png', weapon: fists }),
   };
 }
 
